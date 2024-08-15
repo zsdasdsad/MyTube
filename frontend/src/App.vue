8f09ad951b99dce2b0f1257369104d4239d1a694
zsdasdsad <au30286@gmail.com>
2024-08-15 16:51:42 +0500

    router setup and change icon

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,12 @@
 <script setup>
+  import { RouterView } from 'vue-router';
 
 </script>
 
 <template>
-
+  <div class="view">
+    <RouterView/>
+  </div>
 </template>
 
 <style scoped>
---
+++ frontend/src/components/MainPage.vue
@@ -0,0 +1,19 @@
+<script setup>
+
+</script>
+
+<template>
+  <div>
+    <button>12</button>
+  </div>
+</template>
+
+<style scoped>
+button {
+  background-color: blue;
+  color: white;
+  padding: 10px;
+  border: none;
+  border-radius: 5px;
+}
+</style>
