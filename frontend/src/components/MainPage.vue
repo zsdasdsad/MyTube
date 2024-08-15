2f466a88cec743059aa5c01d4908819dab806f36
zsdasdsad <au30286@gmail.com>
2024-08-16 00:26:03 +0500

    added search bar and logo on main page

--- frontend/src/components/MainPage.vue
+++ frontend/src/components/MainPage.vue
@@ -4,16 +4,48 @@
 
 <template>
   <div>
-    <button>12</button>
+    <div>
+      <img src="../assets/harosh.jpg" alt="Youtube Logo" class="main_logo" />
+      <h1>MyTube</h1>
+    </div>
+    <div class="header">
+      <input type="text" placeholder="Search..." class="search-bar" />
+    </div>
   </div>
 </template>
 
 <style scoped>
-button {
-  background-color: blue;
-  color: white;
+
+.main_logo {
+  width: 50px;
+  height: 50px;
+  position: absolute;
+  top: 15px;
+  left: 15px;
+}
+h1 {
+  font-size: 2rem;
+  margin: 0;
+  padding: 0;
+  position: absolute;
+  top: 23px;
+  left: 75px;
+  font-family: sans-serif;
+}
+.search-bar {
+  width: 500px;
   padding: 10px;
-  border: none;
-  border-radius: 5px;
+  font-size: 1rem;
+  border: 1px solid #ccc;
+  border-radius: 100px;
+}
+.header {
+  position: fixed;
+  top: 0;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  padding: 10px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
 }
 </style>
